01378a9d7f496ad2b1e35a83349a312f246f878b
charles <charles0610@hotmail.fr>
2025-11-13 12:28:08 +0100

    question cyber

--- quizcyber.go
+++ quizcyber.go
@@ -5,6 +5,16 @@ import (
 )
 
 func StartQuizCyber() {
-	fmt.Println("=== Quiz Informatique ===")
-	// plus tard : poser les questions ici
+	fmt.Println("=== Quiz cybersécurité ===")
+	// question 1
+	fmt.Println("Que signifie HTTPS ?")
+	fmt.Println("1. HyperText Transfer Protocol Secure")
+	fmt.Println("2. HyperText Transfer Protocol Standard")
+	fmt.Println("3. HyperText Transfer Protocol Simple")
+	var answer1 int
+	fmt.Println("Que signifie le sigle « VPN » ?")
+	fmt.Println("1. Virtual Private Network")
+	fmt.Println("2. Virtual Public Network")
+	fmt.Println("3. Virtual Protected Network")
+	var answer2 int
 }
