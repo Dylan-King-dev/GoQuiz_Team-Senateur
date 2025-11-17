e4de8ad76ccdc1f1c009c1e24a02ac1066ca10a9
charles <charles0610@hotmail.fr>
2025-11-17 11:07:31 +0100

    quest data/cyber

--- quizIAdata.go
+++ quizIAdata.go
@@ -4,5 +4,28 @@ import "fmt"
 
 func StartQuizIAData() {
 	fmt.Println("=== Quiz IA/Data ===")
-	// plus tard : poser les questions ici
+	score := 0
+
+	fmt.Println("Question 1 : que signifie “donnée structurée” ?")
+	fmt.Println("1. Une donnée organisée en lignes et colonnes")
+	fmt.Println("2. Une image")
+	fmt.Println("3. Un texte libre")
+	if CheckAnswer(1) {
+		score = score + 1
+	}
+	fmt.Println("Question 2: Que signifie “SQL”")
+	fmt.Println("1. Super Quick Language")
+	fmt.Println("2. Structured Query Language")
+	fmt.Println("3. Simple Question List")
+	if CheckAnswer(2) {
+		score = score + 1
+	}
+	fmt.Println("Question 3 : Un “dataset” est :")
+	fmt.Println("1. Une vidéo")
+	fmt.Println("2. Un ensemble de données")
+	fmt.Println("3. Une application")
+	if CheckAnswer(2) {
+		score = score + 1
+	}
+	CalculateScore(score, 3)
 }
--- quizcyber.go
+++ quizcyber.go
@@ -6,17 +6,28 @@ import (
 
 func StartQuizCyber() {
 	fmt.Println("=== Quiz cybersécurité ===")
-	// question 1
-	fmt.Println("Que signifie HTTPS ?")
-	fmt.Println("1. HyperText Transfer Protocol Secure")
-	fmt.Println("2. HyperText Transfer Protocol Standard")
-	fmt.Println("3. HyperText Transfer Protocol Simple")
-	var answer1 int
-	fmt.Println("Que signifie le sigle « VPN » ?")
+	score := 0
+
+	fmt.Println("Question 1: Que signifie “VPN” ?")
 	fmt.Println("1. Virtual Private Network")
-	fmt.Println("2. Virtual Public Network")
-	fmt.Println("3. Virtual Protected Network")
-	var answer2 int
-	fmt.Println("=== Quiz Cybersécurité ===")
-	// plus tard : poser les questions ici
+	fmt.Println("2. Very Protected Network")
+	fmt.Println("3. Virtual Password Number")
+	if CheckAnswer(1) {
+		score = score + 1
+	}
+	fmt.Println("Question 2: Un antivirus sert à :")
+	fmt.Println("1. Créer des mots de passe")
+	fmt.Println("2. Protéger contre les logiciels malveillants")
+	fmt.Println("3. Augmenter la vitesse de l’ordinateur")
+	if CheckAnswer(2) {
+		score = score + 1
+	}
+	fmt.Println("Question 3 : Que signifie “https” dans une adresse web ?")
+	fmt.Println("1. Site non sécurisé")
+	fmt.Println("2. Connexion chiffrée")
+	fmt.Println("3. Jeu en ligne")
+	if CheckAnswer(2) {
+		score = score + 1
+	}
+	CalculateScore(score, 3)
 }
